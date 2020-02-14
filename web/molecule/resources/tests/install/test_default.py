0e331a5409e6eae1669de451eb0054654045dbf9
TL67360N <thibault-externe.lecoq@enedis.fr>
2020-02-14 16:30:00 +0100

    init test playbook with molecule

---
+++ web/molecule/resources/tests/install/test_default.py
@@ -0,0 +1,11 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_users_groups(host):
+    root = host.group('root')
+    assert root.exists
+
